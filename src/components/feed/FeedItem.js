136add81aa6fdec97a0c1c1fa6926c5a6ff09a82
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2019-08-20 11:59:01 -0300

    Finalizando FeedItem

--- src/components/feed/FeedItem.js
+++ src/components/feed/FeedItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View, StyleSheet, Text, Image, Dimensions, TouchableHighlight } from 'react-native';
+import { View, StyleSheet, Text, Image, Dimensions, TouchableHighlight } from 'react-native';
 
 export default class FeedItem extends Component {
 
@@ -8,34 +8,48 @@ export default class FeedItem extends Component {
 
         let rawDate = this.props.data.date_posted.split(' ');
         let date = rawDate[0].split('-');
-        date = date[2]+'/'+date[1]+'/'+date[0];
+        date = date[2] + '/' + date[1] + '/' + date[0];
 
         let time = rawDate[1].split(':');
-        time = time[0]+':'+time[1];
+        time = time[0] + ':' + time[1];
 
         this.state = {
-            dateFormated:date+''+time,
-            screenWidth:Dimensions.get('window').width
+            dateFormated: date + '' + time,
+            screenWidth: Dimensions.get('window').width,
+            photoClickCount: 0,
         }
     }
 
     userClick = () => {
-        this.props.nav.navigate('Profile',{
-            name:this.props.data.name,
-            id:this.props.data.user_id,
+        this.props.nav.navigate('Profile', {
+            name: this.props.data.name,
+            id: this.props.data.user_id,
         });
         // alert("clicou no usuario: "+this.props.data.id_user)
     }
 
     commentUserClick = (name, id) => {
         this.props.nav.navigate('Profile', {
-            name:name,
-            id:id
+            name: name,
+            id: id
         });
     }
 
     photoClick = () => {
-        alert("clicou na foto: "+this.props.data.id)
+        let state = this.state;
+        state.photoClickCount++;
+        this.setState(state)
+
+        if (state.photoClickCount === 2) {
+            this.directLikeClick();
+        }
+
+        setTimeout(() => {
+            let state = this.state;
+            state.photoClickCount = 0;
+            this.setState(state)
+
+        }, 500);
     }
 
     directLikeClick = () => {
@@ -52,13 +66,13 @@ export default class FeedItem extends Component {
                 <View style={styles.feedHeader}>
                     <View style={styles.avatar}>
                         <TouchableHighlight onPress={this.userClick}>
-                            <Image source={{uri:this.props.data.avatar}} style={styles.avatarImg} />
+                            <Image source={{ uri: this.props.data.avatar }} style={styles.avatarImg} />
                         </TouchableHighlight>
-                        
+
                     </View>
                     <View style={styles.userName}>
                         <TouchableHighlight underlayColor={null} onPress={this.userClick}>
-                            <Text>{this.props.data.name}</Text>
+                            <Text>{this.props.data.name} {this.state.photoClickCount}</Text>
                         </TouchableHighlight>
                     </View>
                     <View style={styles.dateArea}>
@@ -68,35 +82,34 @@ export default class FeedItem extends Component {
                 </View>
                 <View style={styles.feedBody}>
                     <TouchableHighlight activeOpacity={1} underlayColor={null} onPress={this.photoClick}>
-                        <Image resizeMode="cover" source={{uri:this.props.data.url}} style={{width:this.state.screenWidth, height:this.state.screenWidth}} />
+                        <Image resizeMode="cover" source={{ uri: this.props.data.url }} style={{ width: this.state.screenWidth, height: this.state.screenWidth }} />
                     </TouchableHighlight>
                 </View>
                 <View style={styles.feedFooter}>
-                <TouchableHighlight underlayColor={null}  onPress={this.directLikeClick} >
-                    <View style={styles.likeArea} >
-                        <Image source={require('../../assets/like_off.png')} style={styles.footerIcon} />
-                        <Text style={styles.likeText}>{this.props.data.like_count}</Text>
-                    </View>
-                </TouchableHighlight>
-                <TouchableHighlight underlayColor={null} onPress={this.toggleCommentArea}>
-                    <View style={styles.commentArea}>
-                        <Image source={require('../../assets/comments.png')} style={styles.footerIcon} />
-                        <Text style={styles.likeText}>{this.props.data.comments.length}</Text>
-                    </View>
-                </TouchableHighlight>
+                    <TouchableHighlight underlayColor={null} onPress={this.directLikeClick} >
+                        <View style={styles.likeArea} >
+                            <Image source={require('../../assets/like_off.png')} style={styles.footerIcon} />
+                            <Text style={styles.likeText}>{this.props.data.like_count}</Text>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight underlayColor={null} onPress={this.toggleCommentArea}>
+                        <View style={styles.commentArea}>
+                            <Image source={require('../../assets/comments.png')} style={styles.footerIcon} />
+                            <Text style={styles.likeText}>{this.props.data.comments.length}</Text>
+                        </View>
+                    </TouchableHighlight>
                 </View>
                 {this.props.data.comments.length > 0 &&
                     <View style={styles.commentContainer}>
-                        {this.props.data.comments.map((citem)=> {
+                        {this.props.data.comments.map((citem) => {
                             return (
-                                    <View style={styles.commentItem}>
-                                        <TouchableHighlight underlayColor={null} 
-                                        onPress={() => 
-                                        { this.commentUserClick(citem.id_user, citem.name)}} style={styles.commentItemUser}>
-                                            <Text>{citem.name}: </Text>
-                                        </TouchableHighlight>
-                                        <Text>{citem.txt}</Text>
-                                    </View> 
+                                <View style={styles.commentItem}>
+                                    <TouchableHighlight underlayColor={null}
+                                        onPress={() => { this.commentUserClick(citem.id_user, citem.name) }} style={styles.commentItemUser}>
+                                        <Text>{citem.name}: </Text>
+                                    </TouchableHighlight>
+                                    <Text>{citem.txt}</Text>
+                                </View>
                             )
                         })}
                     </View>
@@ -110,90 +123,90 @@ export default class FeedItem extends Component {
 }
 
 const styles = StyleSheet.create({
-    feedContainer:{
-        width:'100%',
+    feedContainer: {
+        width: '100%',
     },
-    feedHeader:{
-        height:70,
-        flexDirection:'row',
-        alignItems:'center',
+    feedHeader: {
+        height: 70,
+        flexDirection: 'row',
+        alignItems: 'center',
     },
-    avatar:{
-        width:40,
-        height:40,
-        marginLeft:10,
-        marginRight:15,
-        borderRadius:20,
+    avatar: {
+        width: 40,
+        height: 40,
+        marginLeft: 10,
+        marginRight: 15,
+        borderRadius: 20,
     },
-    avatarImg:{
-        width:40,
-        height:40,
+    avatarImg: {
+        width: 40,
+        height: 40,
     },
-    userName:{
-        width:150,
-        height:25,
-        fontSize:14
+    userName: {
+        width: 150,
+        height: 25,
+        fontSize: 14
     },
-    dateArea:{
-        flex:1,
+    dateArea: {
+        flex: 1,
         flexDirection: 'row',
         justifyContent: 'flex-end',
         alignItems: 'center',
     },
-    clockIcon:{
-        width:15,
-        height:15,
-        marginRight:5,
+    clockIcon: {
+        width: 15,
+        height: 15,
+        marginRight: 5,
     },
-    postDate:{
-        height:20,
-        marginRight:10,
-        fontSize:14,
+    postDate: {
+        height: 20,
+        marginRight: 10,
+        fontSize: 14,
     },
-    feedBody:{
+    feedBody: {
         flex: 1,
-        backgroundColor:'#EEEEEE'
+        backgroundColor: '#EEEEEE'
     },
-    feedFooter:{
-        height:60,
+    feedFooter: {
+        height: 60,
         flexDirection: 'row',
         justifyContent: 'flex-end'
     },
-    likeArea:{
+    likeArea: {
         flexDirection: 'row',
-        height:60,
+        height: 60,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingLeft:20,
-        paddingRight:20
+        paddingLeft: 20,
+        paddingRight: 20
     },
-    commentArea:{
+    commentArea: {
         flexDirection: 'row',
-        height:60,
+        height: 60,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingLeft:20,
-        paddingRight:20,
+        paddingLeft: 20,
+        paddingRight: 20,
     },
-    footerIcon:{
-        width:25,
-        height:25,
-        marginRight:10,
+    footerIcon: {
+        width: 25,
+        height: 25,
+        marginRight: 10,
     },
-    likeText:{
-        fontSize:18,
+    likeText: {
+        fontSize: 18,
     },
-    commentContainer:{
-        padding:10,
+    commentContainer: {
+        padding: 10,
     },
-    commentItem:{
+    commentItem: {
         flexDirection: 'row',
     },
-    feedLineEnd:{
-        height:1,
+    feedLineEnd: {
+        height: 1,
         backgroundColor: '#CCC',
     },
-    commentItemUser:{
-        marginRight:5,
+    commentItemUser: {
+        marginRight: 5,
     }
 });
